f46143544c7408d191e05a1b9c22d5be9df13fc0
chilupa <ichilupa@gmail.com>
2021-06-21 17:52:06 -0700

    Update tests for useBreakpoint hook

--- src/hooks/__tests__/useBreakpoint.test.js
+++ src/hooks/__tests__/useBreakpoint.test.js
@@ -1,10 +1,50 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useBreakpoint from '../useBreakpoint';
+import { act } from 'react-dom/test-utils';
 
 describe('useBreakpoint hook', () => {
-  it('should return breakpoint based on device width', () => {
+  it('should handle xs breakpoint', async () => {
+    act(() => {
+      global.innerWidth = 300;
+      global.dispatchEvent(new Event('resize'));
+    });
+    const { result } = renderHook(() => useBreakpoint());
+    expect(result.current).toBe('xs');
+  });
+
+  it('should handle sm breakpoint', async () => {
+    act(() => {
+      global.innerWidth = 700;
+      global.dispatchEvent(new Event('resize'));
+    });
+    const { result } = renderHook(() => useBreakpoint());
+    expect(result.current).toBe('sm');
+  });
+
+  it('should handle md breakpoint', async () => {
+    act(() => {
+      global.innerWidth = 1000;
+      global.dispatchEvent(new Event('resize'));
+    });
     const { result } = renderHook(() => useBreakpoint());
     expect(result.current).toBe('md');
-    expect(typeof result.current).toBe('string');
+  });
+
+  it('should handle lg breakpoint', async () => {
+    act(() => {
+      global.innerWidth = 1300;
+      global.dispatchEvent(new Event('resize'));
+    });
+    const { result } = renderHook(() => useBreakpoint());
+    expect(result.current).toBe('lg');
+  });
+
+  it('should handle xl breakpoint', async () => {
+    act(() => {
+      global.innerWidth = 1920;
+      global.dispatchEvent(new Event('resize'));
+    });
+    const { result } = renderHook(() => useBreakpoint());
+    expect(result.current).toBe('xl');
   });
 });
